a2de54c39af284f96f8f0bf2cdaa7efa9c90b844
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-09-08 12:52:59 +0000

    linked_functions1.c

---
+++ linked_functions1.c
@@ -0,0 +1,139 @@
+#include "monty.h"
+
+/**
+ * d_linked_len - returns the number of nodes in a doubly linked list
+ * @h: pointer to the list
+ *
+ * Return: number of nodes
+ */
+size_t d_linked_len(const d_linked_t *h)
+{
+	size_t nodes = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
+
+/**
+ * add_d_node - adds a new node at the beginning of a doubly linked list
+ * @head: double pointer to the list
+ * @n: data to insert in the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+d_linked_t *add_d_node(d_linked_t **head, const int n)
+{
+	d_linked_t *new;
+
+	if (!head)
+		return (NULL);
+
+	new = malloc(sizeof(d_linked_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+
+	new->next = *head;
+	new->prev = NULL;
+
+	if (*head)
+		(*head)->prev = new;
+
+	*head = new;
+
+	return (new);
+}
+
+/**
+ * print_d_linked - prints a doubly linked list
+ * @h: pointer to the list
+ *
+ * Return: number of nodes in the list
+ */
+size_t print_d_linked(const d_linked_t *h)
+{
+	size_t nodes = 0;
+
+	if (!h)
+		return (0);
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nodes++;
+	}
+
+	return (nodes);
+}
+
+/**
+ * delete_d_node_index - deltes a node in a doubly linked list
+ * at a given index
+ * @head: double pointer to the list
+ * @index: index of the node to delete
+ *
+ * Return: 1 on success, -1 on failure
+ */
+int delete_d_node_index(d_linked_t **head, unsigned int index)
+{
+	d_linked_t *temp = *head;
+	unsigned int i = 0;
+
+	if (!index)
+	{
+		(*head) = temp->next;
+		if (temp->next)
+			temp->next->prev = NULL;
+		temp->next = NULL;
+		free(temp);
+		return (1);
+	}
+
+	while (i < index)
+	{
+		temp = temp->next;
+		i++;
+		if (!temp)
+			return (0);
+	}
+
+	temp->prev->next = temp->next;
+	if (temp->next)
+		temp->next->prev = temp->prev;
+	free(temp);
+
+	return (1);
+}
+
+/**
+ * get_d_node_nth_index - gets the nth node of a doubly linked list
+ * @head: pointer to the list
+ * @index: index of the node to return
+ *
+ * Return: address of the node, or if it does not exist, NULL
+ */
+d_linked_t *get_d_node_nth_index(d_linked_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (!head)
+		return (NULL);
+
+	while (head && i < index)
+	{
+		head = head->next;
+		i++;
+	}
+
+	return (head ? head : NULL);
+}
